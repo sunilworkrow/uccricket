3278dd0760a3be25c7f504ff82f65e7e3af60c29
sunilworkrow <ksunil@workrow.io>
2025-01-16 16:59:00 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
 const nextConfig = {
-  reactStrictMode: true,
-};
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
+}
+
 
 export default nextConfig;
